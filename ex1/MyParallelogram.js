d22c27132fad0d9336e160db1b46fab6b269bdbc
Luís Tavares <luistavares10@outlook.pt>
2020-02-27 15:47:02 +0000

    Finished exercises from the last exercise sheet

---
+++ ex1/MyParallelogram.js
@@ -0,0 +1,40 @@
+/**
+ * MyParallelogram
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyParallelogram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			1, 1, 0,	//0
+			0, 0, 0,	//1
+            1, 0, 0,	//2
+            2, 0, 0,    //3
+            3, 1, 0,    //4
+            2, 1, 0     //5
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 5,
+			3, 4, 5,
+			2, 1, 0,
+            3, 2, 0,
+            5, 3, 0,
+            5, 4, 3
+        ];
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
+
